eea4172080ebbeeaad0ee7fddababff1f02eef30
Michael Watts <wattsjmichael@gmail.com>
2020-08-10 15:05:22 -0700

    capitalizes first and last letter
    Co-authored-by: Chloe Hellberg <chloe.hellberg@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
 let userSentence = prompt("Enter a sentence")
 
-function lastLetter() {
-  return userSentence.charAt(userSentence.length-1);
-}
-
-
 function firstLetter() {
-return (userSentence.charAt(0));
+  return (userSentence.charAt(0)).toUpperCase();
 }
 
-console.log(firstLetter()+lastLetter());
+
+function lastLetter() {
+  return userSentence.toUpperCase().charAt(userSentence.length-1)
+}
+
+// function upperCase(){
+//   return (firstLetter.toUpperCase())h;
+// }
+
+alert(firstLetter()+lastLetter());
